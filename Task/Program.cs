83009d44ffc337617163b9d89e547be6514853dd
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-22 08:59:41 +0300

    Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+
+int CountEven(int[] array)
+{
+    int count = 0;
+    foreach (int item in array)
+        if (item % 2 == 0)
+            count++;
+    return count;
+}
+
+try
+{
+    Console.Write("Введите длину массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    if (length < 0)
+        Console.WriteLine("Введена некоректная длина массива");
+    else
+    {
+        int[] arr = new int[length];
+        FillArray(arr);
+        PrintArray(arr);
+        Console.WriteLine($"Количество чётных чисел в массиве равно {CountEven(arr)}");
+    }
+}
+catch
+{
+    Console.WriteLine("Введена некоректная длина массива");
+}
