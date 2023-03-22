544bb13341883ad742aa49e4fb1cf6ebd6be98e4
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-22 17:13:46 +0300

    Задача VERYHARD

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача VERY HARD необязательная Имеется массив случайных целых чисел. Создайте массив, в который попадают числа, описывающие максимальную возрастающую последовательность. 
+// Порядок элементов менять нельзя.
+// Одно число - это не последовательность.
+// Пример:
+// [1, 5, 2, 3, 4, 6, 1, 7] => [1, 7]
+// [1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1 ] => [1, 5]
+// [1, 5, 3, 4, 1, 7, 8 , 15 , 1 ] => [3, 5]
+
+int Enter(string message)
+{
+    Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] Array(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        array[i] = new Random().Next(min, max+1);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+    Console.WriteLine();
+}
+
+int[] Subsequence(int[] arr);
+{
+    int left = 0;
+    int right = 0;
+    int poiner = 0;
+    
+}
+
+int length = Enter("Введите длину массива: ");
+int min = Enter("Введите минимально возможное значение для элементов массива: ");
+int max = Enter("Введите максимально возможное значение для элементов массива: ");
+int[] arr = Array(length, min, max);
+PrintArray(arr);
