cd7ca33b67c8de3fed5688c5e5a49b8cc370788e
iegorov99 <egorov.1999.ilya@yandex.ru>
2023-03-24 12:04:06 +0300

    не сделал

--- Task/Program.cs
+++ Task/Program.cs
@@ -6,38 +6,38 @@
 // [1, 5, 2, 3, 4, 1, 7, 8 , 15 , 1 ] => [1, 5]
 // [1, 5, 3, 4, 1, 7, 8 , 15 , 1 ] => [3, 5]
 
-int Enter(string message)
-{
-    Console.Write(message);
-    int result = Convert.ToInt32(Console.ReadLine());
-    return result;
-}
+// int Enter(string message)
+// {
+//     Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
 
-int[] Array(int length, int min, int max)
-{
-    int[] array = new int[length];
-    for (int i = 0; i < length; i++)
-        array[i] = new Random().Next(min, max+1);
-    return array;
-}
+// int[] Array(int length, int min, int max)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < length; i++)
+//         array[i] = new Random().Next(min, max+1);
+//     return array;
+// }
 
-void PrintArray(int[] array)
-{
-    foreach (int item in array)
-        Console.Write($"{item} ");
-    Console.WriteLine();
-}
+// void PrintArray(int[] array)
+// {
+//     foreach (int item in array)
+//         Console.Write($"{item} ");
+//     Console.WriteLine();
+// }
 
-int[] Subsequence(int[] arr);
-{
-    int left = 0;
-    int right = 0;
-    int poiner = 0;
+// int[] Subsequence(int[] arr);
+// {
+//     int left = 0;
+//     int right = 0;
+//     int poiner = 0;
     
-}
+// }
 
-int length = Enter("Введите длину массива: ");
-int min = Enter("Введите минимально возможное значение для элементов массива: ");
-int max = Enter("Введите максимально возможное значение для элементов массива: ");
-int[] arr = Array(length, min, max);
-PrintArray(arr);
+// int length = Enter("Введите длину массива: ");
+// int min = Enter("Введите минимально возможное значение для элементов массива: ");
+// int max = Enter("Введите максимально возможное значение для элементов массива: ");
+// int[] arr = Array(length, min, max);
+// PrintArray(arr);
